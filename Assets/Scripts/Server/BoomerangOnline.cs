704e84b447ad5616aba08e032acddd61750bd81c
LordFrodo <mfvelsaa@hotmail.com>
2022-05-01 16:47:13 -0500

    boomerang online avances

--- Assets/Scripts/Server/BoomerangOnline.cs
+++ Assets/Scripts/Server/BoomerangOnline.cs
@@ -5,9 +5,28 @@ using Photon.Pun;
 
 public class BoomerangOnline : Player2_Boomerang,IPunObservable
 {
+    bool m_isFiring;
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        throw new System.NotImplementedException();
+        if (stream.IsWriting)
+        {
+            stream.SendNext(m_isFiring);
+        }
+        else
+        {
+            this.m_isFiring = (bool)stream.ReceiveNext();
+        }
+    }
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        myBoomerang.DeactiveColider += DeactivateCol;
+        //map_Manager = FindObjectOfType<Map_Manager>();
+        myCollider = GetComponent<CapsuleCollider>();
+        //alive = true;
+        myBoomerang.target = transform;
+        mov = GetComponent<Movement>();
+        //map_Manager.Mapchanger += Activatecollider;
     }
 
     // Start is called before the first frame update
