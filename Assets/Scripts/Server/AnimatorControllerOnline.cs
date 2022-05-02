c98d662dd8f52e7ed49ac1c246e1c2e8e4c66b3c
LordFrodo <mfvelsaa@hotmail.com>
2022-05-01 21:30:57 -0500

    Animator Controller Online Implementado & Dash Online Fixed

---
+++ Assets/Scripts/Server/AnimatorControllerOnline.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class AnimatorControllerOnline : MonoBehaviour
+{
+    public delegate void AnimatorEvents();
+    public event AnimatorEvents Fall;
+    Animator anim;
+    MovementOnline mov;
+    [Range(0, 1)]
+    [SerializeField] int animation_type;
+    LookAtOnline look;
+    Button shoot_bttn;
+
+    PhotonView pv;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        pv = GetComponentInParent<PhotonView>();
+    }
+    void Start()
+    {
+        mov = GetComponentInParent<MovementOnline>();
+        anim = GetComponent<Animator>();
+        look = GetComponent<LookAtOnline>();
+        anim.SetInteger("Animation_type", 1);
+        shoot_bttn = GameObject.FindGameObjectWithTag("Shoot").GetComponent<Button>();
+        shoot_bttn.onClick.AddListener(Throw_Anim);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (pv.IsMine)
+        {
+            Debug.Log(mov.running);
+            if (mov.running) anim.SetBool("Running", true);
+            else anim.SetBool("Running", false);
+        }
+    }
+    public void Throw_Anim()
+    {
+        anim.SetTrigger("Throw");
+    }
+    public void Die()
+    {
+        anim.SetTrigger("Die");
+        mov.die = true;
+        look.death = true;
+        Invoke("Disable", 2f);
+    }
+    void Disable()
+    {
+        gameObject.SetActive(false);
+        if (anim.GetBool("Falling")) anim.SetBool("Falling", false);
+
+    }
+    void Change_Map()
+    {
+        mov.die = false;
+        look.death = false;
+        gameObject.SetActive(true);
+        anim.SetInteger("Animation_type", animation_type);
+        Map_Manager.change_mp = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Water")
+        {
+            if (Fall != null) Fall();
+            anim.SetBool("Falling", true);
+            mov.die = true;
+            Invoke("Disable", 2f);
+        }
+    }
+
+}
--- Assets/Scripts/Server/DashOnline.cs
+++ Assets/Scripts/Server/DashOnline.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 
-public class DashOnline : Dash, IPunObservable
+public class DashOnline : MonoBehaviour, IPunObservable
 {
+    [SerializeField] private float dash_time, dash_speed, initial_speed, cooldown, current_time;
+    Transform player;
+    protected bool dash_enable;
+
     Button btn;
+    MovementOnline mov;
+    PhotonView pv;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
             stream.SendNext(dash_enable);
+            stream.SendNext(transform.position);
         }
         else
         {
             this.dash_enable = (bool)stream.ReceiveNext();
+            this.transform.position = (Vector3)stream.ReceiveNext();
         }
     }
 
@@ -24,6 +37,55 @@ public class DashOnline : Dash, IPunObservable
     {
         btn = GameObject.FindGameObjectWithTag("Dash").GetComponent<Button>();
         btn.onClick.AddListener(Star_Dash);
+        if (pv.IsMine)
+        {
+            player = transform.GetChild(2).GetComponent<Transform>();
+            mov = GetComponent<MovementOnline>();
+            current_time = cooldown;
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (current_time >= cooldown) dash_enable = true;
+        else current_time += Time.deltaTime;
+        /*if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Dash_coroutine());
+        }*/
+    }
+    IEnumerator Dash_coroutine()
+    {
+        float start_time = Time.time;
+        while (Time.time < start_time + dash_time)
+        {
+            Vector3 target_pos = transform.position + player.forward * dash_speed * Time.deltaTime;
+            target_pos = new Vector3(target_pos.x, transform.position.y, target_pos.z);
+            RaycastHit raycastHit;
+            Physics.Raycast(transform.position, player.forward * dash_speed * Time.deltaTime, out raycastHit, 2f);
+            if (raycastHit.collider == null)
+            {
+                //Can move
+                transform.position = target_pos;
+            }
+            yield return null;
+        }
+
+    }
+    public void Star_Dash()
+    {
+        if (dash_enable)
+        {
+            current_time = 0;
+            dash_enable = false;
+            StartCoroutine(Dash_coroutine());
+        }
+
     }
 
 }
