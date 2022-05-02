cd0d6bcda570c8597683a062f9340e196be9e621
majo <60201796+IMeitonI@users.noreply.github.com>
2022-05-01 20:22:45 -0500

    Nickname save

--- Assets/Scripts/UI/LoadSaveNickname.cs
+++ Assets/Scripts/UI/LoadSaveNickname.cs
@@ -6,15 +6,11 @@ using Photon.Pun;
 public class LoadSaveNickname : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    { Debug.Log("si lo estoy haceido");
-        
-    }
+ 
 
     // Update is called once per frame
     public void SaveNickname()
     {
-        Save_Manager.saveM_instance.activeSave.nickname = PhotonNetwork.NickName;
-        Save_Manager.saveM_instance.Save();
+       
     }
 }
