c54c3db599b3e0fbd0f6a568ee6484f5784b025b
Marcelo Alvarenga Tamayo <Marcelo.alvarenga@upb.edu.co>
2022-05-15 11:01:39 -0500

    Aim Casi terminado
    Falta nuevo UI

--- Assets/Scripts/UI/HoldButton.cs
+++ Assets/Scripts/UI/HoldButton.cs
@@ -9,10 +9,10 @@ public class HoldButton : MonoBehaviour
 {
     Movement mov;
     [SerializeField] GameObject aim_arrow;
-    float time;
+    float time, anim_time;
     float duration = 0.2f;
     bool hold;
-    float duration_anim;
+    float duration_anim = 0.5f;
   
 
     public void Aim(bool x)
@@ -22,6 +22,7 @@ public class HoldButton : MonoBehaviour
         else
         {
             time = 0;
+            anim_time = 0;
             hold = false;
             aim_arrow.SetActive(x);
             mov.Aim(x);
@@ -29,7 +30,14 @@ public class HoldButton : MonoBehaviour
     }
     private void Update()
     {
-        if (hold == true) time += Time.deltaTime;
+        if (hold == true)
+        {
+            time += Time.deltaTime;
+            anim_time += Time.deltaTime;
+            float temp = anim_time / duration_anim;
+            aim_arrow.transform.localScale = Vector3.Lerp(Vector3.zero, new Vector3(0.3f,1,0.5f), temp);
+        }
+
         if (time >= duration)
         {
             aim_arrow.SetActive(true);
