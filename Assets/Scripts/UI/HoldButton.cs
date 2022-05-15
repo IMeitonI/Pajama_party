841744b10e2de2e3af9b6dafe1e6f4edd79158c2
Marcelo Alvarenga Tamayo <Marcelo.alvarenga@upb.edu.co>
2022-05-15 10:52:20 -0500

    Aim V1

--- Assets/Scripts/UI/HoldButton.cs
+++ Assets/Scripts/UI/HoldButton.cs
@@ -8,12 +8,33 @@ using UnityEngine.Events;
 public class HoldButton : MonoBehaviour
 {
     Movement mov;
+    [SerializeField] GameObject aim_arrow;
+    float time;
+    float duration = 0.2f;
+    bool hold;
+    float duration_anim;
   
 
     public void Aim(bool x)
     {
         mov = GetComponent<Movement>();
-        mov.Aim(x);
+        if (x == true) hold = true;
+        else
+        {
+            time = 0;
+            hold = false;
+            aim_arrow.SetActive(x);
+            mov.Aim(x);
+        }
+    }
+    private void Update()
+    {
+        if (hold == true) time += Time.deltaTime;
+        if (time >= duration)
+        {
+            aim_arrow.SetActive(true);
+            mov.Aim(true);
+        }
     }
 
 }
