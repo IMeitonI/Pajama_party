da32984fa0c8baa48ad8608be07d10d07d9efb2e
Marcelo Alvarenga Tamayo <Marcelo.alvarenga@upb.edu.co>
2022-05-14 22:47:31 -0500

    Ground Check Frodo

--- Assets/Scripts/Player/GroundCheckl.cs
+++ Assets/Scripts/Player/GroundCheckl.cs
@@ -6,11 +6,15 @@ using System;
 public class GroundCheckl : MonoBehaviour
 {
     public static bool grounded;
-    RaycastHit rhit;
+    RaycastHit[] hits = new RaycastHit[4];
+    Vector3[] array = new Vector3[4];
     // Start is called before the first frame update
     private void Start()
     {
-        Physics.Raycast(this.transform.position, Vector3.down, out rhit, 4);
+        array[0] = new Vector3(-1,0,0);
+        array[1] = new Vector3(1, 0, 0);
+        array[2] = new Vector3(0, 0, -1);
+        array[3] = new Vector3(0, 0, 1);
     }
     // Update is called once per frame
     void Update()
@@ -19,6 +23,11 @@ public class GroundCheckl : MonoBehaviour
     }
     public bool CheckGround()
     {
-        return rhit.collider != null;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            Physics.Raycast(transform.position + array[i], Vector3.down, out hits[i], 6f);
+            if (hits[i].collider != null && hits[i].collider.CompareTag("Ground")) return true;
+        }
+        return  false;
     }
 }
