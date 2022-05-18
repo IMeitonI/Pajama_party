8b6fdc4becc2435bc668f4b435ff7a6c7e1a6275
LordFrodo <mfvelsaa@hotmail.com>
2022-05-18 01:05:14 -0500

    Falling mejorado & rotation Map Implementado

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -45,6 +45,7 @@ public class Movement : MonoBehaviour
     public bool falling, aiming;
     GroundCheckl check;
     Dash dash;
+    Rigidbody rg;
 
     public bool firstTimeFalling = true;
 
@@ -55,6 +56,7 @@ public class Movement : MonoBehaviour
     {
         check = GetComponent<GroundCheckl>();
         dash = GetComponent<Dash>();
+        rg = GetComponent<Rigidbody>();
         multiplier_speed = 1;
     }
     void FixedUpdate()
@@ -97,7 +99,11 @@ public class Movement : MonoBehaviour
                 die = true;
                 falling = true;
                 splashPS.Play();
+                rg.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+                Collider temp = GetComponent<Collider>();
+                temp.enabled = false;
                 transform.rotation = Quaternion.Euler(Vector3.zero);
+                transform.parent = null;
             }
         }
 
@@ -128,7 +134,6 @@ public class Movement : MonoBehaviour
     }
     public void Change_Pos(float x, float z)
     {
-        //Debug.Log(gameObject.name + check.grounded);
 
          if (aiming == false && multiplier_speed > 0 && check.grounded)
         {
@@ -137,13 +142,15 @@ public class Movement : MonoBehaviour
             Vector3 target_pos = transform.position + force * speed * Time.deltaTime* multiplier_speed;
             target_pos = new Vector3(target_pos.x, transform.position.y, target_pos.z);
             RaycastHit raycastHit;
-            Physics.Raycast(transform.position, force, out raycastHit, 4 * speed * Time.deltaTime);
+            Physics.Raycast(transform.position, force, out raycastHit, 4 * speed * Time.deltaTime); rg.constraints = RigidbodyConstraints.FreezePosition;
             if (raycastHit.collider == null && Map_Manager.change_mp == false)
             {
                 transform.position = target_pos;
+                rg.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
             }
             else
             {
+                rg.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
                 //Cannot move horizontally
                 Vector3 second_move_dir = new Vector3(0, 0, z);
                 target_pos = transform.position + second_move_dir * speed * Time.deltaTime;
