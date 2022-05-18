1b0bcba9079ae2bc447da5854730ec26ce3518ac
María José <maria.ramirezll@upb.edu.co>
2022-05-18 11:28:52 -0500

    Retoques mapas

--- Assets/Scripts/Moving_Obj.cs
+++ Assets/Scripts/Moving_Obj.cs
@@ -6,17 +6,7 @@ public class Moving_Obj : MonoBehaviour
 {
     [SerializeField]
     Transform movingObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     private void OnCollisionEnter(Collision collision)
     {
